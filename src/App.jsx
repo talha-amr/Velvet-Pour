8497457eb3aa6c595544f687adcd451d0684fc98
Muhammad Talha Amir <tellyamir16@gmail.com>
2025-08-19 16:19:46 +0500

    added loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import './App.css'
 import About from './components/About'
 import Art from './components/Art'
@@ -8,21 +7,41 @@ import Hero from './components/Hero'
 import Menu from './components/Menu'
 import Navbar from './components/Navbar'
 import gsap from 'gsap'
-import { ScrollTrigger,SplitText } from 'gsap/all'
-gsap.registerPlugin(ScrollTrigger,SplitText)
+import { ScrollTrigger, SplitText } from 'gsap/all'
+import { useState, useEffect } from 'react'
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const handleLoad = () => setIsLoading(false)
+    window.addEventListener('load', handleLoad)
+    return () => window.removeEventListener('load', handleLoad)
+  }, [])
 
   return (
     <>
-      <Navbar/>
-      <Hero/>
-      <Cocktails/>
-      <About/>
-      <Art/>
-       {/* <div className="h-dvh bg-black"></div>  */}
-       <Menu/>
-       <Contact/>
+      {isLoading && (
+        <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black">
+          <img
+            src="/images/logo.png"
+            alt="Velvet Pour"
+            className="w-20 h-20 animate-pulse"
+          />
+        </div>
+      )}
+
+      <div className={`${isLoading ? 'opacity-0' : 'opacity-100'} transition-opacity duration-700`}>
+        <Navbar/>
+        <Hero/>
+        <Cocktails/>
+        <About/>
+        <Art/>
+        <Menu/>
+        <Contact/>
+      </div>
     </>
   )
 }
