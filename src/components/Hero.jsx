7358f608b0893e143b11fbf8d4dab5eb079773d7
Muhammad Talha Amir <tellyamir16@gmail.com>
2025-08-20 13:35:10 +0500

    added react responsive package

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -73,7 +73,7 @@ const Hero = ({ startAnimations }) => {
                 <img className='left-leaf' alt='left-leaf' src='/images/hero-left-leaf.png' />
                 <img className='right-leaf' alt='right-leaf' src='/images/hero-right-leaf.png' />
                 
-                <div className="absolute md:bottom-20 bottom-30 top-auto md:top-[30vh] w-full left-0">
+                <div className="absolute md:bottom-20 bottom-40 w-full left-0">
                     <div className="container-content">
                         {/* Desktop Layout - Left and Right Content */}
                         <div className="md:flex md:justify-between hidden">
@@ -109,15 +109,15 @@ const Hero = ({ startAnimations }) => {
                 </div>
             </section>
             
-            <div className="video absolute inset-0 hidden md:block">
-                <video
-                    ref={vidRef}
-                    muted
-                    playsInline
-                    preload="auto"
-                    src="/videos/output.mp4"
-                />
-            </div>
+        <div className="video absolute inset-0 hidden md:block">
+            <video
+                ref={vidRef}
+                muted
+                playsInline
+                preload="auto"
+                src="/videos/output.mp4"
+            />
+        </div>
         </div>
     )
 }
